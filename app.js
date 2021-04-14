7d6682e612094cfde43f1aeb5d66624d4efa4be4
aprin418 <aprin418@gmail.com>
2021-04-14 11:38:35 -0400

    learn booleans and conditionals

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log(5+5);
+
+console.log("I don't like anchovies");
+
+let age = "30";
+
+console.log(age);
+
+console.log(parseInt(age));
+
+let name = "Aaron";
+
+console.log(name);
+
+console.log(name.replace("a", "A"));
+
+console.log(5===5);
+
+let doesEqual = 5===5;
+console.log(doesEqual);
+
+let stopLight = "red";
+
+if (stopLight === "green") {
+    console.log("GO!");
+} else if (stopLight === "red") {
+    console.log("STOP!");
+}    else {
+        console.log("Slow Down");
+}
+
