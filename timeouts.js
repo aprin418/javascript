167698c3815a6a8c96b7a6fed76d27b841dac191
aprin418 <aprin418@gmail.com>
2021-04-16 12:11:06 -0400

    countdown attempt

---
+++ timeouts.js
@@ -0,0 +1,26 @@
+function countDown() {
+    for (i = 10; i > 0; i-- ) {
+    let timer = [i];
+    }
+    console.log[i];
+
+    setTimeout (countDown, 10000);
+}
+
+
+
+
+
+
+// function alarmRing() {
+//     console.log('RRRIIINNNGGGGG');
+// }
+
+// function turnOffSnooze() {
+//     console.log("turning snooze off now");
+//     clearTimeout(snoozeAlarm);
+// }
+
+// const snoozeAlarm = setTimeout(alarmRing, 3000);
+
+// const snoozeOff = setTimeout(turnOffSnooze, 10000);
